225f72b0a3fc5ee90c4e4a2142e818600190badc
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-30 05:39:05 +0100

    create fetch items endpoint

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const Item = require("./item");
 
 const app = express();
 
 const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
+
+app.get("/items", async (req, res) => {
+  try {
+    const items = await Item.findAll();
+    res.json(items);
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
