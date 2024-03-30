f3a722c16bc992362470a9d4bf1a6919d3a4260c
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-30 16:04:49 +0100

    add login endpoint

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const Item = require("./item");
+const User = require("./user");
 
 const app = express();
 
 const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
+
+app.post("/login", async (req, res) => {
+  const { enteredEmail, enteredPassword } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email: enteredEmail } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      enteredPassword,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 app.get("/items", async (req, res) => {
   try {
