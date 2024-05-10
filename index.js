e189bb5f4cbe9fc5893eec4310b0d92bc1e97b4f
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-10 22:42:52 +0200

    add verifyToken function to verify JWT validity

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(403).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
 app.post("/login", async (req, res) => {
   const { enteredEmail, enteredPassword } = req.body;
 
